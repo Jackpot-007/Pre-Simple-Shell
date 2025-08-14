b82f7d0a5fc970f25daa8b6617ac859fd5239173
EzequielKH <laudarosa12@gmail.com>
2025-08-14 14:47:38 +0000

    made_getline.c_file

---
+++ getline.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int main(int ac, char **av) {
+	
+	char *prompt = "(messi) $ ";
+	char *lineptr;
+	size_t n = 0;
+
+
+	(void)ac; (void)av;
+
+
+	printf("%s", prompt);
+	getline(&lineptr, &n, stdin);
+	printf("%s", lineptr);
+
+	free(lineptr); 
+	return (0);
+}
---
+++ main_getline.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+
+
+int main(int ac, char **av) {
+
+   return (0);
+}
