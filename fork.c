b70cbf1af537235b6a38ac5b94dbfe765aef7781
EzequielKH <laudarosa12@gmail.com>
2025-08-14 17:36:43 +0000

    wawa

---
+++ fork.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - fork 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_pid;
+    pid_t pid;
+
+    return (0);
+}
--- getline.c
+++ getline.c
@@ -6,9 +6,7 @@ int main(int ac, char **av) {
 	char *lineptr;
 	size_t n = 0;
 
-
 	(void)ac; (void)av;
-
 
 	printf("%s", prompt);
 	getline(&lineptr, &n, stdin);
