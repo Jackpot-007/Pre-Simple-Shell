21bb116542079e9b3896ffe188d9a93b22a026ad
Jackpot-007 <jackpotmerlon@gmail.com>
2025-08-19 01:52:41 +0000

    sans

---
+++ prompt.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+ * prompt - procesa la linea de entrada,
+ * crea un proceso hijo y ejecuta el comando ingresado.
+ *
+ * @line: linea de entrada del usuario (cadena de caracteres).
+ *
+ * Return: 0 en caso de exito.
+ */
+int prompt(char *line)
+{
+	char *argv[10]; /* Arreglo para almacenar los argumentos del comando */
+	int i = 0; /* Contador para iterar sobre los argumentos */
+	char *token; /* Puntero para almacenar cada segmento de la linea */
+
+	token = strtok(line, " \n"); /* Divide la linea en tokens separados por espacio */
+	while (token != NULL && i < 9) /* Itera mientras haya tokens y espacio en el arreglo */
+	{
+		argv[i++] = token; /* Guarda cada token en argv */
+		token = strtok(NULL, " \n"); /* Obtiene el siguiente token en la linea */
+	}
+	argv[i] = NULL; /* Termina el arreglo con NULL como pide execvp */
+
+	if (fork() == 0) /* Crea un proceso hijo para ejecutar el comando */
+	{
+		execvp(argv[0], argv);  /* Proceso hijo: ejecuta el comando con sus argumentos */
+
+		perror("execvp failed"); /* Si execvp falla, se muestra un error y termina el hijo */
+		exit(EXIT_FAILURE); /* Termina el proceso hijo con un codigo de fallo */
+	}
+	else /* Se ejecuta en el proceso padre */
+	{
+		wait(NULL); /* El padre espera a que el proceso hijo termine */
+	}
+
+	return (0); /* Retorna 0 en caso de exito de la funcion */
+}
